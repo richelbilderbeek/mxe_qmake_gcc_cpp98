67df540ed7b26466dd98f0b659bb8fb215e3e74a
richelbilderbeek <richel@richelbilderbeek.nl>
2018-04-13 12:57:23 +0200

    Install all packages

--- install_mxe.sh
+++ install_mxe.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Install the MXE prerequisites"
-sudo apt-get install autopoint gperf intltool p7zip-full
+sudo apt-get install autopoint gperf intltool p7zip-full bison flex libtool scons
 
 echo "Clone the GitHub"
 git clone https://github.com/mxe/mxe.git
